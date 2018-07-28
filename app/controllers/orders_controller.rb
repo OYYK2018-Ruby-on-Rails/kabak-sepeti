22c9e14c626b376f110832d16cb8bfea00be1cab
anil <anil@iyidogan.com>
2018-07-28 18:59:44 +0300

    refactor spec

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,12 @@
 class OrdersController < ApplicationController
   def create
-    order = Order.create
+    order = Order.create(order_params)
     render json: order
+  end
+
+  private
+
+  def order_params
+    params.permit(:total, :email, :status)
   end
 end
--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -4,18 +4,19 @@ RSpec.describe OrdersController, type: :request do
   let(:body) do
     {
       email: 'kamp@rails.com',
-      total: 64.0,
-      status: :pending
+      total: 64,
+      status: 'pending'
     }
+  end
+
+  let(:fields) { %w(id email total status updated_at created_at) }
+
+  def json
+    JSON.parse(response.body)
   end
 
   def post_request
     post('/orders', params: body)
-  end
-
-  it 'return should 200' do
-    post_request
-    expect(response).to have_http_status(200)
   end
 
   it 'should create order' do
@@ -24,8 +25,29 @@ RSpec.describe OrdersController, type: :request do
     end.to change { Order.count }.by(1)
   end
 
-  it 'return should email' do
-    post_request
-    expect(JSON.parse(response.body)['email']).to eq body[:email]
+  context 'return should' do
+    before do
+      post_request
+    end
+
+    it '200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'email' do
+      expect(json['email']).to eq body[:email]
+    end
+
+    it 'status' do
+      expect(json['status']).to eq body[:status]
+    end
+
+    it 'total' do
+      expect(json['total'].to_f).to eq body[:total]
+    end
+
+    it 'fields' do
+      expect(json.keys).to match_array(fields)
+    end
   end
 end
